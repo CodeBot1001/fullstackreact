ed7cd763f0223c06f91fba0222f54d950c61fae7
Cody Bailey <cody.bailey.m@gmail.com>
2018-04-19 19:06:48 +0000

    d

--- config/prod.js
+++ config/prod.js
@@ -2,9 +2,13 @@
 //DEV.JS DONT COMMIT THIS
 //can make these available to other files within our application
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: 'mongodb://codyprod:password@ds251889.mlab.com:51889/emaily-prod',
+    //googleClientID: process.env.GOOGLE_CLIENT_ID,
+    //googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     //mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    //cookieKey: process.env.COOKIE_KEY
+    
+    googleClientID: '233231259510-fo0mmhg8tqpl0omsd2opr6075544r49n.apps.googleusercontent.com',
+    googleClientSecret: 'V73TNceUjBektXDScvI79grW',
+    mongoURI: 'mongodb://cody:password@ds249269.mlab.com:49269/emaily-dev',
+    cookieKey: 'asdflakjsdfhlaskjdfhaslkfjhaslfkjh'
 };
